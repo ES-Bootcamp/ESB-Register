3ce2ebdb2e5e304db98b2267bfd80e7bc0a70aec
celyes <celyes02@gmail.com>
2020-10-09 16:06:48 +0100

    update index

--- index.php
+++ index.php
@@ -1,11 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to ESBootcamp</title>
-</head>
-<body>
-    <?php require 'config/database.php'; ?>
-</body>
-</html>
+index here
