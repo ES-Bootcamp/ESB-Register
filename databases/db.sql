4add7d1651f1defca1466c99b0548ea601290bc9
celyes <celyes02@gmail.com>
2020-10-10 15:38:22 +0100

    update db.sql

--- databases/db.sql
+++ databases/db.sql
@@ -19,9 +19,9 @@
 -- Current Database: `ESBRegister`
 --
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `ESBRegister` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+/*CREATE DATABASE /*!32312 IF NOT EXISTS `ESBRegister` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
-USE `ESBRegister`;
+/*USE `ESBRegister`;*/
 
 --
 -- Table structure for table `users`
