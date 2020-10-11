c3d316aa9cf385acd773062aef5ddadcf3579ca1
celyes <celyes02@gmail.com>
2020-10-11 20:26:18 +0100

    create login page view

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="#">ESB-Registration</a>
+        <a class="navbar-brand" href="index.php">ESB-Registration</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -16,10 +16,10 @@
         </ul>
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-            <a class="nav-link" href="/login.php">Login</a>
+            <a class="nav-link" href="login.php">Login</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="/register.php">Register</a>
+            <a class="nav-link" href="register.php">Register</a>
             </li>
         </ul>
         </div>
--- login.php
+++ login.php
@@ -1,1 +1,43 @@
-login here
+<?php require 'includes/header.php'; ?>
+
+
+<div class="container pt-5">
+    <div class="row">
+        <div class="col-md-6 offset-md-3">
+            <form action="" id="login-form" class="p-5">
+                <h3>Login to your account</h3>
+                <br>
+                <div class="row">
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="user-input">
+                                <span class="fa fa-user"></span>
+                                Choose a username
+                            </label>
+                            <input type="text" class="form-control" id="user-login-input" placeholder="Username">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="password-input">
+                                <span class="fa fa-lock"></span>
+                                Pick a strong password
+                            </label>
+                            <input type="text" class="form-control" id="password-login-input" placeholder="Password">
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="row mt-3">
+                    <div class="col">
+                        <button type="submit" class="btn btn-lg btn-primary" id="login-button">Login</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php require 'includes/footer.php'; ?>
