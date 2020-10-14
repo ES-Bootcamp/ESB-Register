199c91ca3786dc7e82306a82be09be320e6ade28
celyes <celyes02@gmail.com>
2020-10-14 23:14:54 +0100

    refactor dashboard

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,1 +1,11 @@
-Welcome, user
+<?php 
+session_start();
+$_SESSION['redirected_from_dashboard'] = true;
+if(!isset($_SESSION['is_logged_in'])) header('Location: ../login.php'); exit;
+require '../includes/header.php'; 
+?>
+<form action="../actions/logout.action.php" method="POST">
+    <button type="submit">Log out</button>
+</form>
+
+<?php require '../includes/footer.php';
