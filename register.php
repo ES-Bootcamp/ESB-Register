5112dbe8a7f015d9b522d70d21e4536109d861a0
allache abdelmalek <allacheabdelmalek@gmail.com>
2020-10-18 13:52:34 -0400

    update register actiuon nd register index

--- register.php
+++ register.php
@@ -2,7 +2,7 @@
 <div class="container pt-5">
     <div class="row">
         <div class="col-md-6 offset-md-3">
-            <form action="" id="register-form" class="p-5">
+            <form action="actions/register.action.php" id="register-form" class="p-5" method="POST">
                 <h3>Login to your account</h3>
                 <br>
                 <div class="row">
@@ -12,7 +12,7 @@
                                 <span class="fa fa-user"></span>
                                 Username
                             </label>
-                            <input type="text" class="form-control" id="user-register-input" placeholder="Username">
+                            <input type="text" class="form-control" id="user-register-input" placeholder="Username" name="username">
                         </div>
                     </div>
                 </div>
@@ -23,7 +23,7 @@
                                 <span class="fa fa-lock"></span>
                                 Password
                             </label>
-                            <input type="text" class="form-control" id="password-register-input" placeholder="Password">
+                            <input type="text" class="form-control" id="password-register-input" placeholder="Password" name="password" value="">
                         </div>
                     </div>
                 </div>
@@ -36,7 +36,7 @@
                                 <span class="fa fa-lock"></span>
                                 Repeat password
                             </label>
-                            <input type="text" class="form-control" id="re-password-register-input" placeholder="Repeast password">
+                            <input type="text" class="form-control" id="re-password-register-input" placeholder="Repeat password" name="rpassword" value="">
                         </div>
                     </div>
                 </div>
@@ -47,7 +47,7 @@
                                 <span class="fa fa-at"></span>
                                 Your email
                             </label>
-                            <input type="text" class="form-control" id="email-register-input" placeholder="email">
+                            <input type="text" class="form-control" id="email-register-input" placeholder="email" name="email">
                         </div>
                     </div>
                 </div>
@@ -57,6 +57,18 @@
                         <button type="submit" class="btn btn-lg btn-primary" id="register-button">Register</button>
                     </div>
                 </div>
+                <?php 
+               // $pass=$_POST["password"];
+                //$rpass=$_POST["rpassword"];
+           
+                        if($_POST["password"]!=$_POST["rpassword"]){
+                        ?>
+                         <div class="alert alert-danger mt-3" role="alert">
+                            the password repeat is not equel 
+                        </div>
+                        <?php
+                        }
+                        ?>
             </form>
         </div>
     </div>
